319f7011aa9635a4e8ddce9214c103fd37181034
Toni <tonilocke@hotmail.it>
2025-01-02 11:45:33 +0100

    random quotes generator

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState,useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+function App() {
+  const [quotes,setQuotes] = useState([]);
+  const [randomQuote,setRandomQuote] = useState({"quote":"Generate a quote","author":"by Author Name"});
+  async function fetchDB(){
+    const response = await fetch("http://localhost:5173/data/db.json");
+    const result = await response.json();
+    setQuotes(result.quotes);
+  }
+  useEffect(()=>{
+    setTimeout(()=>{
+      fetchDB();
+    },1000);
+  },[]); 
+  
+  function newRandomQuote(){
+    const rand =Math.floor( Math.random() * quotes.length);
+    setRandomQuote(quotes[rand]);
+    console.log(randomQuote);
+  }
+  return (
+    <div id="quote-box">
+      <p id="text"> { randomQuote.quote} </p>
+      <p id="author">- { randomQuote.author } </p>
+      <button id="new-quote" onClick={()=>newRandomQuote()}>New Quote</button>
+      <a href="twitter.com/intent/tweet" target="_blank" id="tweet-quote">Tweet</a>
+    </div>
+  )
+}
+
+export default App;
